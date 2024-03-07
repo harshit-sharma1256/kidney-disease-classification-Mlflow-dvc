8ee8e3226f142d7aec077e06a0271d1c503ae57b
Harshit Sharma <harshit2531937@gmail.com>
2024-03-07 16:21:15 +0530

    Model Downloading and preparation completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -8,3 +8,14 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)  #This means i don't want to add anything else here.It will throw an error if used try to add something here.
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
